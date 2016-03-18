49bc26f08a855293581fbd299dd7aa8c598de809
Jorge Ovalle <jroz9105@gmail.com>
2016-03-18 15:00:20 -0500

    Pods, first icons

---
+++ DoorConcept/Controllers/Generic/GenericTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GenericTabBarViewController.swift
+//  DoorConcept
+//
+//  Created by Jorge Raul Ovalle Zuleta on 3/18/16.
+//  Copyright © 2016 jorgeovalle. All rights reserved.
+//
+
+import UIKit
+
+class GenericTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor.purpleColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'DoorConcept' do
+pod 'SESlideTableViewCell'
+pod 'FlatUIKit'
+pod 'Ophiuchus', '~> 1.0.3'
+end
+
+target 'DoorConceptTests' do
+
+end
+
